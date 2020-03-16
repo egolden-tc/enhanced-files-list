5c1875c2d4149a00628d97a7c477298093630677
Caitlin Marshall <caitlin.marshall@gmail.com>
2020-03-15 20:05:34 -0400

    Updates for getting the file URL asynchronously

--- force-app/main/default/lwc/filesList/filesList.js
+++ force-app/main/default/lwc/filesList/filesList.js
@@ -12,8 +12,8 @@ import {
     NavigationMixin
 } from 'lightning/navigation';
 
-//Datatable Columns
 
+//Datatable Columns
 const columns = [{
     label: 'File Name',
     fieldName: 'nameURL',
@@ -41,7 +41,7 @@ const columns = [{
 
 
 export default class filesList extends NavigationMixin(LightningElement) {
-    @track data = [];
+    @track files = [];
     @track columns = columns;
 
 
@@ -51,7 +51,7 @@ export default class filesList extends NavigationMixin(LightningElement) {
     @api uploadFiles;
     @api objectApiName;
 
-
+    //Using the wire service to get the records returned by the Apex method
     @wire(getRelatedFilesByRecordId, {
         recordId: '$recordId'
     })
@@ -59,36 +59,25 @@ export default class filesList extends NavigationMixin(LightningElement) {
         error,
         data
     }) {
+
+        //if data is returned, pass it to getFilesToDisplay method
         if (data) {
 
-            let currentData = [];
-
-            data.forEach((row) => {
-
-
-                let rowData = {};
-
-                rowData.nameURL = '/lightning/r/ContentDocument/' + row.ContentDocumentId + '/view';
-                rowData.Title = row.Title;
-                rowData.Type__c = row.Type__c;
-                rowData.Category__c = row.Category__c
-                rowData.Expiration_Date__c = row.Expiration_Date__c
-
-
-                currentData.push(rowData);
-
-            });
-
-            this.data = currentData;
+            this.getFilesToDisplay(data).then(finalFiles => {
+                //getFilesToDisplay returns a promise, use .then to access the result after it's done
+                this.files = finalFiles;
+            })
         } else if (error) {
             window.console.log(error);
         }
     }
 
+    //Defines the accepted file formats
     get acceptedFormats() {
         return ['.jpg', '.jpeg', '.png', '.svg', '.gif'];
     }
 
+    //Defines success message/behavior for file upload
     handleUploadFinished() {
         this.dispatchEvent(new ShowToastEvent({
             title: `Success`,
@@ -97,6 +86,7 @@ export default class filesList extends NavigationMixin(LightningElement) {
         }));
     }
 
+    //Defines what should happen when View All is clicked--navigate to the View All Files list
     viewAll() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordRelationshipPage',
@@ -108,5 +98,53 @@ export default class filesList extends NavigationMixin(LightningElement) {
             },
         });
     }
+
+    //Use lightning-navigation service to generate the File URL for each File
+    //Asynchronous and returns a promise
+    getURL(fileId) {
+        return this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: fileId,
+                actionName: 'view',
+            },
+        });
+
+    }
+
+    //Takes the data from the wire service and iterate over the list to define the column data and get the URL
+    //Asynchronous and returns a promise
+    getFilesToDisplay = async (sourceFiles) => {
+
+        let updatedFileList = [];
+        await this.asyncForEach(sourceFiles, async (row) => {
+
+            let rowData = {};
+
+            rowData.nameURL = await this.getURL(row.ContentDocumentId);
+            rowData.Title = row.Title;
+            rowData.Type__c = row.Type__c;
+            rowData.Category__c = row.Category__c
+            rowData.Expiration_Date__c = row.Expiration_Date__c
+
+
+            updatedFileList.push(rowData);
+
+        });
+
+        return updatedFileList;
+    }
+
+    //This function is a utility to allow for an asynchronous for each (using await inside the for each block)
+    //This is used in getFilesToDisplay
+    asyncForEach = async (array, callback) => {
+        // iterate passed in array and async function to call for each item
+        for (let index = 0; index < array.length; index++) {
+            // for each item in array, await completion of provided callback
+            await callback(array[index], index, array);
+        }
+    }
+
+
 
 }
