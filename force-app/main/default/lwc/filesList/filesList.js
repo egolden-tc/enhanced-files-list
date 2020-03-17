cbe4f3a63fd75e5feee4eff4f9c91058aea39407
Caitlin Marshall <caitlin.marshall@gmail.com>
2020-03-16 21:17:28 -0400

    updates for sort and view more

--- force-app/main/default/lwc/filesList/filesList.js
+++ force-app/main/default/lwc/filesList/filesList.js
@@ -18,38 +18,47 @@ const columns = [{
     label: 'File Name',
     fieldName: 'nameURL',
     type: 'url',
+    sortable: true,
     typeAttributes: {
         label: {
             fieldName: 'Title'
         },
-        target: '_self',
-        sortable: true
+        target: '_self'
     },
 }, {
     label: 'Type',
     fieldName: 'Type__c',
-    type: 'text'
+    type: 'text',
+    sortable: true
 }, {
     label: 'Category',
     fieldName: 'Category__c',
-    type: 'text'
+    type: 'text',
+    sortable: true
 }, {
     label: 'Expiration Date',
     fieldName: 'Expiration_Date__c',
-    type: 'date'
+    type: 'date',
+    sortable: true
 }];
 
 
-export default class filesList extends NavigationMixin(LightningElement) {
-    @track files = [];
+export default class FilesList extends NavigationMixin(LightningElement) {
+    @track visibleFiles = [];
+    @track hiddenFiles = [];
     @track columns = columns;
+    @track showFooter;
+    @track defaultSortDirection = 'asc';
+    @track sortDirection = 'asc';
+    @track sortedBy;
 
 
     @api recordId;
-    @api footerText = 'View All';
+    @api footerText = 'View More';
     @api icon;
     @api uploadFiles;
     @api objectApiName;
+    @api numFiles;
 
     //Using the wire service to get the records returned by the Apex method
     @wire(getRelatedFilesByRecordId, {
@@ -60,12 +69,20 @@ export default class filesList extends NavigationMixin(LightningElement) {
         data
     }) {
 
-        //if data is returned, pass it to getFilesToDisplay method
+        //if data is returned, check if footer should display and pass data to getFilesToDisplay method
         if (data) {
+
+            if (this.numFiles)
+                this.showFooter = data.length > this.numFiles ? true : false;
 
             this.getFilesToDisplay(data).then(finalFiles => {
                 //getFilesToDisplay returns a promise, use .then to access the result after it's done
-                this.files = finalFiles;
+                if (!this.numFiles) {
+                    this.numFiles = 5;
+                }
+                //get the first numFiles number of finalFiles
+                this.visibleFiles = finalFiles.slice(0, this.numFiles);
+                this.hiddenFiles = finalFiles.slice(this.numFiles);
             })
         } else if (error) {
             window.console.log(error);
@@ -97,6 +114,22 @@ export default class filesList extends NavigationMixin(LightningElement) {
                 actionName: 'view'
             },
         });
+
+    }
+
+    viewMore() {
+        //when clicked, move the next numFiles number of files into visibleFiles from hiddenFiles
+        let newVisibleFiles = this.hiddenFiles.slice(0, this.numFiles);
+        this.visibleFiles = [...this.visibleFiles, ...newVisibleFiles];
+
+        //remove the first numFiles number of files from hiddenFiles by keeping numFiles to the end
+        this.hiddenFiles = this.hiddenFiles.slice(this.numFiles);
+
+        //set showFooter to false if hiddenFiles = 0
+        if (this.hiddenFiles.length === 0) {
+            this.showFooter = false;
+
+        }
     }
 
     //Use lightning-navigation service to generate the File URL for each File
@@ -145,6 +178,47 @@ export default class filesList extends NavigationMixin(LightningElement) {
         }
     }
 
+    //Column sorting
+    onHandleSort(event) {
+        // field name
+        this.sortedBy = event.detail.fieldName;
 
+        // sort direction
+        this.sortDirection = event.detail.sortDirection;
 
+        // calling sortdata function to sort the data based on direction and selected field
+        this.sortData(event.detail.fieldName, event.detail.sortDirection);
+    }
+
+    sortData(fieldname, direction) {
+        // serialize the data before calling sort function
+        let parseData = JSON.parse(JSON.stringify(this.visibleFiles));
+
+        // Return the value stored in the field
+        let keyValue = (a) => {
+
+            if (fieldname === "nameURL") {
+                return a.Title;
+            } else {
+
+                return a[fieldname];
+            }
+        };
+
+        // checking reverse direction 
+        let isReverse = direction === 'asc' ? 1 : -1;
+
+        // sorting data 
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; // handling null values
+            y = keyValue(y) ? keyValue(y) : '';
+
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+
+        // set the sorted data to data table data
+        this.visibleFiles = parseData;
+
+    }
 }
